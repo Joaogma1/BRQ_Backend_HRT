38661a9ee14077eec8d59012293ee7da09044a78
joão gabriel mauricio mauricio <joao.gabriel.mauricio@hotmail.com>
2019-11-12 16:03:45 -0300

    configurado start up

---
+++ BRQ.HRTProject.WebAPI/Extensions/AutoMapperSetup.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BRQ.HRTProject.Aplicacao.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BRQ.HRTProject.WebAPI.Extensions
+{
+    public static class AutoMapperSetup
+    {
+        public static void AddAutoMapperSetup(this IServiceCollection services)
+        {
+            var mapper = AutoMapperConfiguration.ConfigureMappings();
+            services.AddAutoMapper(x => mapper.CreateMapper(), Assembly.Load("BRQ.HRTProject.Aplicacao"));
+        }
+    }
+}
--- BRQ.HRTProject.WebAPI/Startup.cs
+++ BRQ.HRTProject.WebAPI/Startup.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using BRQ.HRTProject.Infra.Data;
+using BRQ.HRTProject.Infra.Ioc;
+using BRQ.HRTProject.WebAPI.Extensions;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Formatter;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BRQ.HRTProject.WebAPI
 {
@@ -24,7 +30,85 @@ namespace BRQ.HRTProject.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "BRQ - HRT", Version = "v1.1" });
+            });
+
+            // String de conexão que o OData utilizará Ao fazer suas Queries
+
+            var conexao = "Data Source = brqsenai.database.windows.net; Initial Catalog = hrt_database; User ID = brqsenai; Password = @Senai132";
+
+            services.AddMvc()
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+            .AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
+
+            // Devemos Criar o contexto antes de descomentar o codigo abaixo
+
+            services.AddDbContext<ContextoHRT>(options =>
+            {
+                options.UseSqlServer(conexao);
+            });
+            services.AddOData();
+
+            // O forEach é responsavel para mapear e dar suporte ao OData no swagger
+            services.AddMvcCore(options =>
+            {
+                foreach (var outputFormatter in options.OutputFormatters.OfType<ODataOutputFormatter>().Where(_ => _.SupportedMediaTypes.Count == 0))
+                {
+                    outputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+                }
+                foreach (var inputFormatter in options.InputFormatters.OfType<ODataInputFormatter>().Where(_ => _.SupportedMediaTypes.Count == 0))
+                {
+                    inputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+                }
+            });
+
+            // Serviço de autenticação (Utilizado na parte "login"(Matricula))
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+
+                    ValidateAudience = true,
+
+                    ValidateLifetime = true,
+
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("L*rKP-x#Fl7-NayO@-Xd!9b")),
+
+                    ClockSkew = TimeSpan.FromHours(2),
+
+                    ValidIssuer = "HTR.WebApi",
+
+                    ValidAudience = "HTR.WebApi"
+                };
+            });
+
+            // Adiciona a politica do Cors
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            // controla a injeção de dependencias no sistema
+            NativeInjectorConfig.RegisterServices(services);
+
+            // Adiciona a configuração do auto mapper
+            services.AddAutoMapperSetup();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +119,23 @@ namespace BRQ.HRTProject.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseAuthentication();
+
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Human Resource Tool API");
+            });
+
+            // configuração da aplicação, habilitada a injeção de DEP algumas funcionalidades do OData
+            app.UseMvc(routeBuilder =>
+            {
+                routeBuilder.EnableDependencyInjection();
+                routeBuilder.Expand().Select().Count().OrderBy().Filter().MaxTop(50);
+            });
         }
     }
 }
