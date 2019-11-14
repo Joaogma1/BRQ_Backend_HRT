887024b6fb13da976d99d461b9d394cda741660a
Paloma Correa <paloma.s@live.com>
2019-11-14 17:05:06 -0300

    login

---
+++ BRQ.HRTProject.Dominio/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using BRQ.HRTProject.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRQ.HRTProject.Dominio.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Usuarios BuscarPorEmailSenha(string email, string senha);
+    }
+}
---
+++ BRQ.HRTProject.Infra.Data/Repositorios/UsuarioRepository.cs
@@ -0,0 +1,22 @@
+﻿using BRQ.HRTProject.Dominio.Entidades;
+using BRQ.HRTProject.Dominio.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BRQ.HRTProject.Infra.Data.Repositorios
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public Usuarios BuscarPorEmailSenha(string email, string senha)
+        {
+            using (ContextoHRT ctx = new ContextoHRT())
+            {
+                Usuarios usuarioBuscado = ctx.Usuarios.Include(y => y.FkPessoaNavigation).Include(x => x.FkTipoUsuarioNavigation).Where(x => x.Email == email && x.Senha == senha).FirstOrDefault();
+                return usuarioBuscado;
+            }
+        }
+    }
+}
